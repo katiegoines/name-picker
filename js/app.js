d10437110c737ae7f36b31f5459e2b74ced8aff5
Philippe Luchansky <philuchansky@gmail.com>
2016-05-26 12:04:11 -0700

    random name picker function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-// 
+var names = [
+	"Joe",
+	"Pat",
+	"Sally"
+]
+
+var picked = []
+
+function pickName(arr){
+	picked.push(arr.splice(Math.floor(Math.random() * arr.length), 1).join(''))
+	if(names.length < 1) {
+		names = picked.concat()
+		picked = []
+	}
+}
